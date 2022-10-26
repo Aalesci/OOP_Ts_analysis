90104a1a4fa7230a6065af966a387f37866e5937
Aalesci <alberto.consorzio@gmail.com>
2022-10-26 19:19:58 +0200

    testa

---
+++ key.py
@@ -0,0 +1,15 @@
+
+#BINACE
+api_key = 'ydllEdrFm8gGTx6uI3MQKED4p6hjSXNtmYHMKuOIf5iZW7i544sRXhfWLvAxC0OK'
+api_secret = 'FzegXx1GzZFjlUzfeaxKUFlv6qQvlpUVQoduzwNQMSRu2Rm49qoQ1BFd5PETX7Zi' 
+
+
+#TELEGRAM 
+api_telegram = 'https://api.telegram.org/bot5707373377:AAE3FBzg5Ke9bMgu3MqgfQg1rDfHbG2AgEk/sendMessage?chat_id=-666204675&text='
+
+#MYSQL
+api_py_aw = 'F60fagiolini' 
+api_db_user='AAlberto',
+api_db_password = 'T60tortora!',
+api_db_host ='AAlberto.mysql.eu.pythonanywhere-services.com',
+api_db_database='AAlberto$Alberto_db'
